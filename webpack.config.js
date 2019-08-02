7352b03fdd813ad6be631cf65930901fb4d98f6f
Ramesh Ramalingam <rrama@thoughtworks.com>
2019-08-02 14:46:07 +0530

    Configured to deploy files to owas folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 const path = require("path");
+const fs = require("fs");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const manifest = JSON.parse(fs.readFileSync("./manifest.webapp", "utf-8"));
+const distPath = "openmrs/owas/" + manifest.name;
 
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, distPath),
     filename: "main.bundle.js"
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html"
+    }),
+    new CopyPlugin([{ from: "./manifest.webapp", to: "./manifest.webapp" }])
+  ],
   stats: {
     colors: true
   },
